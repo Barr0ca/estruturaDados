5cdd703f599af8d83bb5141f2e3a2ddf36d9a503
Barr0ca <iandearaujogalvao@gmail.com>
2025-04-17 17:11:38 -0300

    primeiro commit

---
+++ i.py
@@ -0,0 +1,22 @@
+def insertion(list, index):
+  while index > 0:
+    if list[index] < list[index - 1]:
+      aux = list[index]
+      list[index] = list[index - 1]
+      list[index - 1] = aux
+    index -= 1
+  return list
+
+def insertionSort(list):
+  i = 1
+  while i < len(list):
+    insertion(list, i)
+    i += 1
+  return list
+
+listaD = [3, 1, 5, 10, 15, 90, 2]
+print("lista desordenada")
+print(listaD)
+listaO = insertionSort(listaD)
+print("lista ordenada")
+print(listaO)
