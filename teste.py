828609fdccd1b50baf5fe1f76d3cee52dc7b5239
Barr0ca <iandearaujogalvao@gmail.com>
2025-04-23 18:18:35 -0300

    elaboração do shellsort utilizando o guia e testes feitos

--- teste.py
+++ teste.py
@@ -1,2 +1,15 @@
-for i in range(5):
-  print(i)
+# list = [17,26,93,44,77,31,54,55,20]
+list = [54,26,93,17,77,31,44,55,20]
+
+gap = len(list) // 2
+while gap > 0:
+  for comeco in range(gap):
+    for i in range(comeco+gap, len(list), gap):
+      valorAtual = list[i]
+      posicao = i
+      if list[posicao-gap]>valorAtual:
+        aux = list[posicao]
+        list[posicao] = list[posicao-gap]
+        list[posicao-gap] = aux
+        
+  gap = gap // 2
