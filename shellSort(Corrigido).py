350ae5b264cea41d0a3bc64c6f86b255061bddcf
Ian Galvão <137799663+Barr0ca@users.noreply.github.com>
2025-04-25 09:45:20 -0300

    Update shellSort(Corrigido).py

--- shellSort(Corrigido).py
+++ shellSort(Corrigido).py
@@ -2,6 +2,8 @@ from math import ceil
 
 def shellSort(list):
   gap = ceil(len(list)/2)
+  if gap%2!=0:
+    gap +=1
   while gap > 0:
     for i in range(0, len(list)-gap):
       valorAtual = list[i]
@@ -9,7 +11,7 @@ def shellSort(list):
         aux = list[i]
         list[i] = list[i+gap]
         list[i+gap] = aux
-    gap -= 1
+    gap //= 2
   return list
 
 listaD = [3,1,5,10,15,90,2]
