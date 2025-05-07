7d0997af11666ec76a768842905596e585d098ec
Barr0ca <iandearaujogalvao@gmail.com>
2025-05-07 09:03:19 -0300

    correção

--- bubbleSort.py
+++ bubbleSort.py
@@ -1,11 +1,11 @@
 def bubbleSort(list):
-    for j in range(len(list)-2):
-        for i in range(len(list)-1):
+    for j in range(len(list)):
+        for i in range(len(list)-j-1):
             if list[i] > list[i+1]:
                 aux = list[i]
                 list[i] = list[i+1]
                 list[i+1] = aux
     return list
 
-lista = [8,10,5,4,1,15]
+lista = [8,10,5,4,1]
 print(bubbleSort(lista))
