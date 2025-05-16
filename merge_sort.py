c0e857609dc2b70a75acde60fb840d701d7405f7
Barr0ca <iandearaujogalvao@gmail.com>
2025-05-16 17:55:54 -0300

    Utilizando apenas um Shell Sort

--- merge_sort.py
+++ merge_sort.py
@@ -3,17 +3,20 @@ from shellSort import shell_sort
 from random import randint
 from desempenho import desempenho
 
+
 def merge_sort(lista: list):
     if len(lista) == 1:
         return lista
-    
+
     metade = len(lista)//2
 
-    return shell_sort(shell_sort(merge_sort(lista[:metade])) + shell_sort(merge_sort(lista[metade:])))
+    return shell_sort(merge_sort(lista[:metade])+merge_sort(lista[metade:]))
+
 
 lista = []
 for i in range(10000):
     lista.append(randint(1, 100000))
-
-desempenho(shell_sort, lista)
 desempenho(merge_sort, lista)
+
+# lista = [3, 1, 5, 15, 10, 90, 3, 1, 5, 15, 10, 90]
+# desempenho(merge_sort, lista)
