8ed01422fd58e0c7ec6fa1d5a784d725b6226a81
Barr0ca <iandearaujogalvao@gmail.com>
2025-06-27 08:16:01 -0300

    Perfumando o print

--- ListaExHashTable/medio/questao10.py
+++ ListaExHashTable/medio/questao10.py
@@ -2,7 +2,7 @@
 
 def ht_calc_media(ht: dict):
     for aluno, notas in ht.items():
-        print(aluno, (sum(notas)/len(notas)))
+        print(f'A média do aluno {aluno} é {(sum(notas)/len(notas))}')
 
 ht = {'Ian': [10,80,30,100], 'Paulo': [30,70,30,80]}
 
