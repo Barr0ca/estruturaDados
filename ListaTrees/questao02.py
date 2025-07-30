2784110549fa042deec0e4c87942a23cf668e146
Barr0ca <iandearaujogalvao@gmail.com>
2025-07-30 10:01:43 -0300

    feat: Adicionando Questão 2 já com função recursiva de inserção

---
+++ ListaTrees/questao02.py
@@ -0,0 +1,19 @@
+from binarytree import bst, Node
+
+arvore = bst(height=2, is_perfect=True)
+
+def insercao(no, valor):
+    if no.value != valor:
+        if valor > no.value:
+            if no.right:
+                insercao(no.right, valor)
+            else:
+                no.right = Node(valor)
+        else:
+            if no.left:
+                insercao(no.left, valor)
+            else:
+                no.left = Node(valor)
+    return arvore
+
+print(insercao(arvore, 10))
