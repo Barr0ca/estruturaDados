0ce3fefb0fb8c25f7473ebed0bc44b9ad8c52dec
Barr0ca <iandearaujogalvao@gmail.com>
2025-07-30 10:01:11 -0300

    fix: Arrumando Questão 1 para fornecer aénas oque pede

--- ListaTrees/questao01.py
+++ ListaTrees/questao01.py
@@ -1,23 +1,5 @@
-from binarytree import bst, Node, get_index
-
-def add_valor(no, valor):
-    if get_index(no, no) != valor:
-        if get_index(valor, valor) > get_index(no, no):
-            if get_index(no, no.rigth):
-                add_valor(get_index(no, no.rigth), valor)
-            else:
-                get_index(no, no.rigth) == Node(valor)
-        else:
-            if get_index(no, no.left):
-                add_valor(get_index(no, no.left), valor)
-            else:
-                get_index(no, no.left) == Node(valor)
-    else:
-        return print('Já existe esse valor.')
-    return valor
+from binarytree import bst
 
 arvore = bst(height=2, is_perfect=True)
 
 print(arvore)
-
-add_valor(7, arvore)
